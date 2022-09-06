8056d6801508c474d2cac82525739ef704dc3a0f
bkopustas <benas.k258@gmail.com>
2022-09-06 15:35:55 +0300

    ClockAngle commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClockAngle{
+
+    internal class Program{
+
+        static void Main(string[] args){
+
+            Console.WriteLine("Hours:");
+            if (!int.TryParse(Console.ReadLine(), out int hour) || hour < 1 || hour > 12){
+                Console.Write("Hours should be a number in range of 1-12");
+                return;
+            }
+
+            Console.WriteLine("Minutes:");
+            if (!int.TryParse(Console.ReadLine(), out int minute) || minute < 0 || minute > 59){
+                Console.Write("Minutes should be a number in range of 0-59");
+                return;
+            }
+
+            Console.WriteLine($"Angle: {GetAngle(hour, minute)} degrees");
+
+        }
+
+        static double GetAngle(int hour, int minute){
+            
+            // 360/12 deg every hour + 360/12/60 deg every minute
+            double hourDegrees = hour * 30 + minute * 0.5;
+            // 360/60 deg every minute
+            double minuteDegrees = minute * 6;
+
+            double angle = Math.Abs(hourDegrees - minuteDegrees);
+            if (angle > 180){
+                angle = 360 - angle;
+            }
+            return angle;
+        }
+    }
+}
